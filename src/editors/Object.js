9bf3731e6d7329ef65752f7640f82f8de69d5cd3
Gernot Vormayr <gvormayr@gmail.com>
2018-03-29 22:06:57 +0200

    Added property editor

--- src/editors/Object.js
+++ src/editors/Object.js
@@ -4,6 +4,104 @@ import chevronBottom from 'open-iconic/svg/chevron-bottom.svg'
 
 import { BaseEditor } from '../Editor.js';
 
+class PropertyEditor extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { value: props.item.active };
+  }
+
+  handleChange = (e) => {
+    this.setState(prevState => {
+      if (!prevState.value)
+        this.props.addProperty(this.props.item.id);
+      else
+        this.props.delProperty(this.props.item.id);
+      return { value: !prevState.value }
+    });
+  }
+
+  render() {
+    const item = this.props.item;
+    const title = item.title || item.id;
+    return (
+      <div className="custom-control custom-checkbox">
+        <input type="checkbox" className="custom-control-input" checked={this.state.value} disabled={item.required} onChange={this.handleChange} />
+        <label className="custom-control-label" onClick={this.handleChange} dangerouslySetInnerHTML={{ __html: title }} ></label>
+      </div>
+    );
+  }
+}
+
+class PropertyDialog extends Component {
+  componentDidMount() {
+    document.addEventListener('mousedown', this.handleClickOutside);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('mousedown', this.handleClickOutside);
+  }
+
+  setWrapperRef = (node) => {
+    this.wrapperRef = node;
+  }
+
+  handleClickOutside = (e) => {
+    if (this.wrapperRef && !this.wrapperRef.contains(e.target) && !this.props.button.contains(e.target)) {
+      this.props.close();
+    }
+  }
+
+  render() {
+    const properties = this.props.properties.map(item => (<PropertyEditor key={item.id} item={item} addProperty={this.props.addProperty} delProperty={this.props.delProperty} />));
+    return (
+      <div className="modal-dialog" ref={this.setWrapperRef}>
+        <div className="modal-content">
+          <div className="modal-body">
+            {properties}
+          </div>
+          <div className="modal-footer">
+            More Stuff
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class PropertyButton extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { open: false };
+  }
+
+  handleOpen = (e) => {
+    this.setState((prevState) => ({ open: !prevState.open }));
+  }
+
+  close = () => {
+    this.setState({ open: false });
+  }
+
+  setWrapperRef = (node) => {
+    this.wrapperRef = node;
+  }
+
+  render() {
+    let classes = "btn btn-sm btn-outline-secondary dropdown-toggle";
+    if (this.state.open) {
+      classes += " active"
+    }
+    return (
+      <div className="mx-2 propertyContainer">
+        <button type="button" className={classes} onClick={this.handleOpen} ref={this.setWrapperRef}>Properties</button>
+        {this.state.open && <PropertyDialog close={this.close} button={this.wrapperRef} properties={this.props.getProperties()} addProperty={this.props.addProperty} delProperty={this.props.delProperty} />}
+      </div>
+    );
+  }
+}
+
 class Chevron extends Component {
   constructor(props) {
     super(props);
@@ -45,12 +143,32 @@ class ObjectEditor extends Component {
     }
   }
 
+  getProperties = () => {
+    const active = Object.keys(this.value);
+    const required = this.props.constraints.required || [];
+    const properties = this.props.constraints.properties || {};
+
+    return [...new Set(Object.keys(properties).concat(active))].map(val => (
+      { id: val, active: active.indexOf(val) >= 0, required: required.indexOf(val) >= 0, title: (properties[val] || {}).title }
+    ));
+  }
+
+  addProperty = (id) => {
+    console.log("add " + id);
+  }
+
+  delProperty = (id) => {
+    console.log("del " + id)
+  }
+
   componentDidMount() {
-    this.props.addPrecontrol("chevron", -1000, <Chevron key="chevron" handleHide={this.handleHide} open={this.state.open} />);
+    this.props.addPrecontrol("chevron", -1000, <Chevron key="objectChevron" handleHide={this.handleHide} open={this.state.open} />);
+    this.props.addPostcontrol("properties", -1000, <PropertyButton key="objectProperty" getProperties={this.getProperties} addProperty={this.addProperty} delProperty={this.delProperty} />);
   }
 
   componentWillUnmount() {
     this.props.delPrecontrol("chevron");
+    this.props.delPostcontrol("properties");
   }
 
   handleHide = (open) => {
