ab54b9565a62b818a56f12ce4d5c10ae37af7210
Gernot Vormayr <gvormayr@gmail.com>
2018-04-07 00:07:31 +0200

    Remove bootstrap css (external)

--- src/Editor.js
+++ src/Editor.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './Editor.css';
 import processSchema from './Schema.js';
 import State from './State.js';
